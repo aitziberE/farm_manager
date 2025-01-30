ac4a3dc8328dff7d0b7a7e92434794fbc167680e
Ander Moreno <amorenoch15@gmail.com>
2025-01-30 17:18:50 +0100

    added default data, relations in view and everything working

--- __manifest__.py
+++ __manifest__.py
@@ -31,7 +31,7 @@
         'views/menus.xml',
         'data/default_data.xml',
 
-        'views/animal_group_views',
+        'views/animal_group_views.xml',
         'views/animal_views.xml',
         'views/species_views.xml',
         'reports/animal_group_report.xml',
